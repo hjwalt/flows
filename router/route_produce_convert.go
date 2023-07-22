6bc94171c06b2d6bf21c9668745ffcc782390a5c
Hady Willi <hady.willi@outlook.com>
2023-07-22 23:26:49 +0800

    feat: adapter route

--- router/route_produce_convert.go
+++ router/route_produce_convert.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hjwalt/flows/message"
 	"github.com/hjwalt/flows/stateless"
+	"github.com/hjwalt/flows/topic"
 	"github.com/hjwalt/runway/format"
 )
 
@@ -34,6 +35,37 @@ func RouteProduceBodyMapConvert[Req any, Key any, Value any](
 		if marshalError != nil {
 			return nil, marshalError
 		}
+
+		return &bytesResMessage, nil
+	}
+}
+
+func RouteProduceTopicBodyMapConvert[Req any, Key any, Value any](
+	source stateless.OneToOneFunction[message.Bytes, Req, Key, Value],
+	requestFormat format.Format[Req],
+	produceTopic topic.Topic[Key, Value],
+) stateless.OneToOneFunction[message.Bytes, message.Bytes, message.Bytes, message.Bytes] {
+	return func(ctx context.Context, m message.Message[[]byte, []byte]) (*message.Message[[]byte, []byte], error) {
+
+		formattedMessage, unmarshalError := message.Convert(m, format.Bytes(), format.Bytes(), format.Bytes(), requestFormat)
+		if unmarshalError != nil {
+			return nil, unmarshalError
+		}
+
+		outputMessage, outputerr := source(ctx, formattedMessage)
+		if outputerr != nil {
+			return nil, outputerr
+		}
+
+		if outputMessage == nil {
+			return nil, nil
+		}
+
+		bytesResMessage, marshalError := message.Convert(*outputMessage, produceTopic.KeyFormat(), produceTopic.ValueFormat(), format.Bytes(), format.Bytes())
+		if marshalError != nil {
+			return nil, marshalError
+		}
+		bytesResMessage.Topic = produceTopic.Topic()
 
 		return &bytesResMessage, nil
 	}
---
+++ runtime_router_adapter.go
@@ -0,0 +1,59 @@
+package flows
+
+import (
+	"github.com/hjwalt/flows/message"
+	"github.com/hjwalt/flows/router"
+	"github.com/hjwalt/flows/runtime_bunrouter"
+	"github.com/hjwalt/flows/runtime_sarama"
+	"github.com/hjwalt/flows/stateless"
+	"github.com/hjwalt/flows/topic"
+	"github.com/hjwalt/runway/format"
+	"github.com/hjwalt/runway/runtime"
+)
+
+// Wiring configuration
+type RouterAdapterConfiguration[Request any, InputKey any, InputValue any] struct {
+	Name                       string
+	ProduceTopic               topic.Topic[InputKey, InputValue]
+	ProduceBroker              string
+	RequestBodyFormat          format.Format[Request]
+	RequestMapFunction         stateless.OneToOneFunction[message.Bytes, Request, InputKey, InputValue]
+	HttpPort                   int
+	KafkaProducerConfiguration []runtime.Configuration[*runtime_sarama.Producer]
+	RouteConfiguration         []runtime.Configuration[*runtime_bunrouter.Router]
+}
+
+func (c RouterAdapterConfiguration[Request, InputKey, InputValue]) Runtime() runtime.Runtime {
+
+	// producer configs
+	kafkaProducerConfigs := []runtime.Configuration[*runtime_sarama.Producer]{
+		runtime_sarama.WithProducerBroker(c.ProduceBroker),
+	}
+	if len(c.KafkaProducerConfiguration) > 0 {
+		kafkaProducerConfigs = append(kafkaProducerConfigs, c.KafkaProducerConfiguration...)
+	}
+
+	// route configs
+	routeConfigs := []runtime.Configuration[*runtime_bunrouter.Router]{
+		runtime_bunrouter.WithRouterPort(c.HttpPort),
+		runtime_bunrouter.WithRouterProducerHandler(
+			runtime_bunrouter.POST,
+			"/"+c.Name,
+			router.RouteProduceTopicBodyMapConvert(
+				c.RequestMapFunction,
+				c.RequestBodyFormat,
+				c.ProduceTopic,
+			),
+		),
+	}
+	if len(c.RouteConfiguration) > 0 {
+		routeConfigs = append(routeConfigs, c.RouteConfiguration...)
+	}
+
+	routerConfiguration := RouterConfiguration{
+		KafkaProducerConfiguration: kafkaProducerConfigs,
+		RouteConfiguration:         routeConfigs,
+	}
+
+	return routerConfiguration.Runtime()
+}
