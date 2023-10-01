e194816b4f909196eec562cbfba6d5d75574c911
Hady Willi <hady.willi@outlook.com>
2023-10-01 19:32:49 +0800

    feat flow produce helper

--- flow/producer.go
+++ flow/producer.go
@@ -3,6 +3,7 @@ package flow
 import (
 	"context"
 
+	"github.com/hjwalt/runway/format"
 	"github.com/hjwalt/runway/structure"
 )
 
@@ -10,4 +11,18 @@ type Producer interface {
 	Produce(context.Context, []Message[structure.Bytes, structure.Bytes]) error
 	Start() error
 	Stop()
+}
+
+func Produce[K any, V any](
+	c context.Context,
+	flowProducer Producer,
+	topic Topic[K, V],
+	m Message[K, V],
+) error {
+	byteMessage, convertErr := Convert(m, topic.KeyFormat(), topic.ValueFormat(), format.Bytes(), format.Bytes())
+	if convertErr != nil {
+		return convertErr
+	}
+	byteMessage.Topic = topic.Name()
+	return flowProducer.Produce(c, []Message[structure.Bytes, structure.Bytes]{byteMessage})
 }
